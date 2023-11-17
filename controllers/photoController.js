86843747dc64ead1a82fbed3615bfd2716b0a090
Vatirsoy <burakkutlukk@gmail.com>
2023-11-17 15:39:27 +0300

    mail

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -36,8 +36,8 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
   try {
     const photos = res.locals.user
-    ? await Photo.find({ user: { $ne: res.locals.user._id } })
-    : await Photo.find({})
+      ? await Photo.find({ user: { $ne: res.locals.user._id } })
+      : await Photo.find({});
 
     res.status(200).render("photos", {
       photos,
@@ -75,7 +75,7 @@ const deletePhoto = async (req, res) => {
     await cloudinary.uploader.destroy(photoId);
     await Photo.findOneAndRemove({ _id: req.params.id });
 
-    res.status(200).redirect('/users/dashboard');
+    res.status(200).redirect("/users/dashboard");
   } catch (error) {
     res.status(500).json({
       succeded: false,
@@ -84,4 +84,40 @@ const deletePhoto = async (req, res) => {
   }
 };
 
-export { createPhoto, getAllPhotos, getAPhoto, deletePhoto };
+const updatePhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById(req.params.id);
+
+    if (req.files) {
+      const photoId = photo.image_id;
+      await cloudinary.uploader.destroy(photoId);
+
+      const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+          use_filename: true,
+          folder: "lenslight_tr",
+        }
+      );
+
+      photo.url = result.secure_url;
+      photo.image_id = result.public_id;
+
+      fs.unlinkSync(req.files.image.tempFilePath);
+    }
+
+    photo.name = req.body.name;
+    photo.description = req.body.description;
+
+    photo.save();
+
+    res.status(200).redirect(`/photos/${req.params.id}`);
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto, deletePhoto, updatePhoto };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,12 +5,16 @@ import {
   getRegisterPage,
   getLoginPage,
   getLogout,
+  getContactPage,
+  sendMail,
 } from "../controllers/pageController.js";
 
 const router = express.Router(); //Create new express router
 
 router.route("/").get(getIndexPage);
 router.route("/about").get(getAboutPage);
+router.route("/contact").get(getContactPage);
+router.route("/contact").post(sendMail);
 router.route("/register").get(getRegisterPage);
 router.route("/login").get(getLoginPage);
 router.route("/logout").get(getLogout);
